abef43b0a12cd01bc34d773fc486230c3e677233
Fabien MICHEL <michelfabien30650@gmail.com>
2023-05-21 17:15:56 +0200

    clean

--- stateTree.py
+++ stateTree.py
@@ -2,38 +2,27 @@
 # 3 actions possible: move, turn 90, turn -90
 # store at each index the value of the total information gained for this path
 # return 1, 2, 3 for the best action
-stateTree = []
-depth = 0
 DEPTH_MAX = 4
-count = 0
-for i in range(DEPTH_MAX + 1):
-    # 3 actions possible
-    for j in range(pow(3, depth)):
-        stateTree.append(count)
-        count += 1
-    depth += 1
-    # print(stateTree)
-# print(stateTree)
-# print(count)
+def createStateTree():
+    stateTree = []
+    depth = 0
+    count = 0
+    for i in range(DEPTH_MAX + 1):
+        # 3 actions possible
+        for j in range(pow(3, depth)):
+            stateTree.append(count)
+            count += 1
+        depth += 1
+    return stateTree
 
-def choiceAction(stateTree, DEPTH_MAX):
-    # print("indexes last level leafs")
-    # print(pow(3, DEPTH_MAX), stateTree[len(stateTree) - pow(3, DEPTH_MAX)])
-    # print(len(stateTree) - 1, stateTree[len(stateTree) - 1])
 
+def choiceAction(stateTree):
+    """
+    return the best action to do
+    @param stateTree: list of the values of the total information gained for each path
+    """
     lastLevelLeafs = stateTree[len(stateTree) - pow(3, DEPTH_MAX):]
-    # print("lastLevelLeafs")
-    # print(lastLevelLeafs)
-
-    # print("max of lastLevelLeafs")
-    # print(max(lastLevelLeafs))
-    # print("index of max of lastLevelLeafs")
-    # print(lastLevelLeafs.index(max(lastLevelLeafs)))
     indexOfMax = lastLevelLeafs.index(max(lastLevelLeafs))
-
-    # print("tresholds")
-    # print(pow(3, DEPTH_MAX - 1), 2 * pow(3, DEPTH_MAX - 1))
-    # print(3 * pow(3, DEPTH_MAX - 1))
 
     if (indexOfMax < pow(3, DEPTH_MAX - 1)):
         return "move"
@@ -41,7 +30,7 @@ def choiceAction(stateTree, DEPTH_MAX):
         return 'turn 90'
     return "turn -90"
 
-
 print("choiceAction")
-print(choiceAction(stateTree, DEPTH_MAX))
+stateTree = createStateTree()
+print(choiceAction(stateTree))
 
