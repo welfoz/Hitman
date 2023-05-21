6a9b9c6e7c3dc65c8b0f1cb33dcaccd01655d4c6
Fabien MICHEL <michelfabien30650@gmail.com>
2023-05-21 17:24:20 +0200

    add information gained function

--- stateTree.py
+++ stateTree.py
@@ -3,25 +3,43 @@
 # store at each index the value of the total information gained for this path
 # return 1, 2, 3 for the best action
 DEPTH_MAX = 4
-def createStateTree():
+
+def createMap(n_col : int, n_lig : int):
+    """
+    Create a map of size n_col * n_lig
+    @param n_col: number of columns
+    @param n_lig: number of lines
+    """
+    map = []
+    for i in range(n_col):
+        map.append([])
+        for j in range(n_lig):
+            map[i].append(0)
+    return map
+
+def createStateTree(map):
+    """
+    computes the total information gained for each path with DEPTH_MAX depth
+    return a list of the values of the total information gained for each path 
+    @param map: the map of the game
+    """
+
     stateTree = []
     depth = 0
-    count = 0
     for i in range(DEPTH_MAX + 1):
         # 3 actions possible
         for j in range(pow(3, depth)):
-            stateTree.append(count)
-            count += 1
+            stateTree.append(informationGained(map, stateTree))
         depth += 1
     return stateTree
 
-
 def choiceAction(stateTree):
     """
-    return the best action to do
+    return the best action to do according to the best path, which maximizes the total information gained
     @param stateTree: list of the values of the total information gained for each path
     """
     lastLevelLeafs = stateTree[len(stateTree) - pow(3, DEPTH_MAX):]
+    print(len(lastLevelLeafs))
     indexOfMax = lastLevelLeafs.index(max(lastLevelLeafs))
 
     if (indexOfMax < pow(3, DEPTH_MAX - 1)):
@@ -30,7 +48,17 @@ def choiceAction(stateTree):
         return 'turn 90'
     return "turn -90"
 
+def informationGained(map, stateTree) -> int:
+    """
+    return the information gained by doing action
+    @param map: the map of the game
+    @param action: the action to do
+    """
+    return 1
+
 print("choiceAction")
-stateTree = createStateTree()
+stateTree = createStateTree(createMap(4, 3))
+print(stateTree)
+print(len(stateTree))
 print(choiceAction(stateTree))
 
