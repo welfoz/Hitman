fed92094a6bac51ddb957235c3d9ebbfe4ff9346
Fabien MICHEL <michelfabien30650@gmail.com>
2023-06-13 17:49:32 +0200

    kill target working

--- main.py
+++ main.py
@@ -491,10 +491,57 @@ def phase2(referee: HitmanReferee, map):
     print("now go kill the target")
 
     ### then go to the target
+    MAX = 100
+    count = 0
+    actions = []
+    goal = targetPosition
+    while count < MAX and not isOnObject(map, position, OBJECTS_INDEX['target']):
+        print("------------------")        
 
+        action = actionChooser.choose_phase2(map, position, goal)
 
+        # all actions 
+        if action == 1:
+            actions.append(('move', position))
+            status = referee.move()
+        elif action == 2:
+            actions.append(("turn 90", position))
+            status = referee.turn_clockwise()
+        elif action == 3:
+            actions.append(("turn -90", position))
+            status = referee.turn_anti_clockwise()
+        elif action == 4:
+            actions.append(("kill_target", position))
+            status = referee.kill_target()
+        elif action == 5:
+            actions.append(("neutralize_guard", position))
+            status = referee.neutralize_guard()
+        elif action == 6:
+            actions.append(("neutralize_civil", position))
+            status = referee.neutralize_civil()
+        elif action == 7:
+            actions.append(("take_weapon", position))
+            status = referee.take_weapon()
+        elif action == 8:
+            actions.append(("take_suit", position))
+            status = referee.take_suit()
+        elif action == 9:
+            actions.append(("put_on_suit", position))
+            status = referee.put_on_suit()
+        else: 
+            raise Exception("action not found")
+
+        orientation = fromHCDirectionToOrientation(status["orientation"])
+        position: Position = (status["position"][0], status["position"][1], orientation)
+        count += 1
+    print("count: ", count)
+    pprint(status)
+
+    print("We are on the target")
     ### then kill the target
-
+    print("we kill the target")
+    status = referee.kill_target()
+    pprint(status)
 
     ### then come back to the start position
 
